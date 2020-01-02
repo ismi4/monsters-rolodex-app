99bc2c413da795aab481f7d25e27a2d7a28b9310
“Ismail <brkicismail@gmail.com>
2020-01-02 12:32:48 +0100

    Updates

---
+++ precache-manifest.3fcfbc71d87071d6c2dc2db345d517f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8453369c2369bf60eb7bf7aff3240cf",
+    "url": "/monsters-rolodex-app/index.html"
+  },
+  {
+    "revision": "c58017061f3ca8d55b80",
+    "url": "/monsters-rolodex-app/static/css/main.ccc6febd.chunk.css"
+  },
+  {
+    "revision": "7f914662a9655b86f49b",
+    "url": "/monsters-rolodex-app/static/js/2.e0f3c114.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex-app/static/js/2.e0f3c114.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c58017061f3ca8d55b80",
+    "url": "/monsters-rolodex-app/static/js/main.29b64199.chunk.js"
+  },
+  {
+    "revision": "5d8f86233241b5a5ec3d",
+    "url": "/monsters-rolodex-app/static/js/runtime-main.94ab0fa6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
